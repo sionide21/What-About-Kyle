7750a6301f50d18b518076a2c2197d034727bef8
Ben Olive <ben.olive@gatech.edu>
2010-03-14 13:29:47 -0400

    Refactored server code

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var listeners = [];
 
 var HOST = null;
 var PORT = 8000;
-var DBHOST = "128.61.115.235";
+var DBHOST = "localhost";
 var DBPORT = 5984;
 var DB = 'cars';
 var LISTENER_TIMEOUT = 30 * 1000;
@@ -28,20 +28,20 @@ var couchdb = require('./lib/couchdb');
 var client = couchdb.createClient(DBPORT, DBHOST);
 var db = client.db(DB);
 
+// key value pairs in the form:
+//   "/path" : callbackFunction
+var paths = {
+"/getCars"    : getCarsForGroup,
+"/addCar"     : addCar,
+"/modifyCar"  : addCar,
+"/deleteCar"  : deleteCar,
+"/listen"     : listen,
+};
+
+// check for expired listeners once per second
+setInterval(clearExpired, 1000);
+
 http.createServer(function (req, res) {
-
-  // key value pairs in the form:
-  //   "/path" : callbackFunction
-  var paths = {
-    "/getCars"    : getCarsForGroup,
-    "/addCar"     : addCar,
-    "/modifyCar"  : addCar,
-    "/deleteCar"  : deleteCar,
-    "/listen"     : listen,
-  };
-
-  // check for expired listeners once per second
-  setInterval(clearExpired, 1000);
     
   // this gets the path part of the url
   // for example, if the user goes to http://localhost/hello?foo=bar
@@ -49,24 +49,34 @@ http.createServer(function (req, res) {
   var parsedUrl = url.parse(req.url, true);
   var path = parsedUrl.pathname;
 
+  function error(code, msg) {
+	log(code + ': ' + path);
+	res.writeHead(code, {'Content-Type': 'text/plain'});
+	res.write(msg);
+	res.close();
+  }
+
   // does the expected path exist?
   if (paths[path]) {
-    log('200: ' + path);
-    paths[path](parsedUrl, function(outJSON) {
-        var jsonCallback = parsedUrl.query.jsoncallback;
+    paths[path](parsedUrl.query, function(outJSON, notifyGroup) {
         outStr = JSON.stringify(outJSON);
-        outStr = jsonCallback + "(" + outStr + ");"
+        var jsonCallback = parsedUrl.query.jsoncallback;
+        if (jsonCallback) {
+            outStr = jsonCallback + "(" + outStr + ");"
+        }
         res.writeHead(200, {
           'Content-Type': 'text/javascript',
           'Content-Length': outStr.length
         });
         res.write(outStr);
         res.close();
-    });
+        if (notifyGroup) {
+            var action = parsedUrl.pathname.substr(1);
+            updateListeners(outJSON, action, notifyGroup);
+        }
+    }, error);
   } else {
-    log('404: ' + path);
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.close();
+    error(404, 'Not Found');
   }
 
 }).listen(PORT);
@@ -84,45 +94,25 @@ function log(output) {
   sys.puts(output);
 }
 
-function addCar(req, res, parsedUrl, responde) {
-  if (parsedUrl.query && parsedUrl.query.car) {
-    var car = parsedUrl.query.car;
+function addCar(query, complete, error) {
+  if (query && query.car) {
+    var car = query.car;
     var carObj = JSON.parse(car);
-    var group = carObj.group;
-    debug("adding car to group: " + group);
-    var jsonCallback = parsedUrl.query.jsoncallback;
     debug('adding car: ' + carObj);
 
     // add the car to the database
-    db.saveDoc(carObj, function(er, doc) {
-      if (er) printError(er);
-      db.getDoc(doc.id, function(er, doc) {
-
-        var action = parsedUrl.pathname.substr(1);
-        var docJson = JSON.stringify(doc);
-        var docStr = jsonCallback + "(" + docJson + ");";
-
-        // return the newly added car to the client that uploaded it
-        res.writeHead(200, {
-          'Content-Type': 'text/javascript',
-          'Content-Length': docStr.length});
-        res.write(docStr);
-        res.close();
-
-        // notify all listening clients
-        debug("pushing " + action + " updates to " + listeners.length + " listeners.");
-        updateListeners(doc, action, group);
-      });
-    });
-
+    db.saveDoc(carObj, handleDb(function(doc) {
+      db.getDoc(doc.id, handleDb(function(doc) {
+        complete(doc, carObj.group);
+      }, error));
+    }, error));
   } else {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.close();
+    error(400, 'Missing required fields');
   }
 }
 
-function deleteCar(req, res, parsedUrl) {
-  var car = parsedUrl.query.car;
+function deleteCar(query, complete, error) {
+  var car = query.car;
   var carObj = JSON.parse(car);
   debug('about to delete');
   debug(car);
@@ -132,48 +122,31 @@ function deleteCar(req, res, parsedUrl) {
   var group = carObj.group;
 
   debug('removing doc: id ' + id + '\nrev ' + rev);
-  db.removeDoc(id, rev, function(er, doc) {
-    if (er) printError(er);
-
-    var jsonCallback = parsedUrl.query.jsoncallback;
-    var action = 'deleteCar';
-    var docStr = jsonCallback + "(" + JSON.stringify(carObj) + ");";
-    debug(docStr);
-
-    res.writeHead(200, {
-      'Content-Type': 'text/javascript',
-      'Content-Length': docStr.length});
-    res.write(docStr);
-    res.close();
-
-    // notify all listening clients
-    updateListeners(carObj, action, group);
-  });
+  db.removeDoc(id, rev, handleDb(function(doc) {
+    complete(carObj, group);
+  }, error));
 }
 
-function getCarsForGroup(parsedUrl, complete) {
+function getCarsForGroup(query, complete, error) {
 
   //TODO get cars for a certain date
 
-  var day = new Date(parsedUrl.query.date);
-  var group = parsedUrl.query.group;
+  var day = new Date(query.date);
+  var group = query.group;
   var queryUrl = '/cars/_design/search/_view/groupsearch?include_docs=true&key="' + group + '"';
   var client = couchdb.createClient(DBPORT, DBHOST);
-  client.request(queryUrl, function(er, docs) {
-    if (er) printError(er);
-
+  client.request(queryUrl, handleDb(function(docs) {
     var retCars = [];
-    for  (var i = 0; i < docs.rows.length; i++) {
+    for (var i = 0; i < docs.rows.length; i++) {
       retCars.push(docs.rows[i].doc);
     }
-    
     complete(retCars);
-  });
+  }, error));
 }
 
-function listen(parsedUrl, complete) {
+function listen(query, complete) {
   //TODO get only on a certain date
-  var group = parsedUrl.query.group;
+  var group = query.group;
 
   // add the listener callback function to the array of listeners
   listeners.push({
@@ -210,10 +183,19 @@ function updateListeners(doc, action, group) {
   }
 }
 
+function handleDb(func,error) {
+    return function(er, doc) {
+      if (er) {
+        printError(er);
+        return error(500, JSON.stringify(er));
+      }
+      func(doc);
+    }
+}
+
 function printError(er) {
   for (var field in er) {
     log(field + ": " + er[field]);
   }
-  throw er;
 }
 
